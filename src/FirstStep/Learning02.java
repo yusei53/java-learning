ca738b2c1e2ed83a5d165bd078cccd824eed66f5
yusei53 <yusei530922@gmail.com>
2024-08-12 18:47:50 +0900

    add: Learning02.java

---
+++ src/FirstStep/Learning02.java
@@ -0,0 +1,14 @@
+package FirstStep;
+import java.util.Scanner;
+
+public class Learning02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("");
+
+        String input = scanner.nextLine();
+
+        System.out.println(input);
+        scanner.close();
+}}
+
