6b90f64888a9d4722cfa488acb12f2123e557c59
yusei53 <yusei530922@gmail.com>
2024-08-12 18:47:44 +0900

    add: Learning01.java

---
+++ src/FirstStep/Learning01.java
@@ -0,0 +1,14 @@
+package FirstStep;
+
+public class Learning01 {
+    public static void main(String[] args) {
+        System.out.print("name: ");
+        System.out.println("今村悠聖");
+
+        System.out.print("language: ");
+        System.out.println("Java");
+
+        System.out.print("hitokoto: ");
+        System.out.println("コンパイラ言語から逃げない");
+    }
+}
