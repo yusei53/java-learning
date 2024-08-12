a325b38202c27c428401978670b5a56f911e7bbe
yusei53 <yusei530922@gmail.com>
2024-08-12 20:06:59 +0900

    add: Learning04.java

---
+++ src/FirstStep/Learning04.java
@@ -0,0 +1,14 @@
+package FirstStep;
+
+import java.util.Scanner;
+
+public class Learning04 {
+    public static void main(String... args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            String S = sc.nextLine();
+            System.out.println(S);
+        }
+    }
+}
